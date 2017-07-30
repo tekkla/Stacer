8f29b5b78d4d0ceb2299a2a2e2ad249aae0448ca
Michael Zorn <tekkla@tekkla.de>
2017-07-30 16:15:13 +0200

    German translation

---
+++ src/languages/langs/lang.de.js
@@ -0,0 +1,81 @@
+/**
+  Michael Zorn <tekkla@tekkla.de>
+*/
+export default {
+	// Top Menu
+	dashboard: "Übersicht",
+	sysCleaner: "Systembereinigung",
+	startupApps: "Autostartprogramme",
+	services: "Dienste",
+	process: "Prozesse",
+	uninstaller: "Deinstallation",
+	resources: "Ressourcen",
+	// Dashboard Page
+	cpu: "PROZESSOR",
+	memory: "SPEICHER",
+	disk: "LAUFWERK",
+	download: "DOWNLOAD",
+	upload: "UPLOAD",
+	systemInfo: "SYSTEMINFORMATIONEN",
+	hostname: "Rechnername",
+	platform: "Plattform",
+	distribution: "Distribution",
+	kernelRel: "Kernelversion",
+	cpuModel: "Prozessor",
+	cpuSpeed: "Takt",
+	cpuCores: "Kerne",
+	// System Cleaner Page
+	packageCache: "Packetzwischenspeicher",
+	crashReports: "Absturzberichte",
+	systemLogs: "Systemlogs",
+	appCache: "Anwendungszwischenspeicher",
+	trash: "Müll",
+	systemScan: "System untersuchen",
+	clean: "Bereinigen",
+	selectAll: "Alle auswählen",
+	// Startup Apps Page
+	startupTitle: "Autostartprogramme",
+	addStartup: "Autostartprogramm hinzufügen",
+	noStartup: "Keine Autostartprogramme gefunden.",
+	app: "Anwendung",
+	appName: "Name",
+	appComment: "Kommentar",
+	appCommand: "Kommando",
+	add: "Hinzufügen",
+	cancel: "Abbruch",
+	// Services Page
+	serviceTitle: "Dienste",
+	noService: "Kein Dienst gefunden",
+	// Process Page
+	endProcess: "Prozess beenden",
+	refresh: "Neu einlesen",
+	// Uninstaller Page
+	uninstallerTitle: "Installierte Pakete",
+	noPackage: "Kein Paket gefunden.",
+	uninstallSelecteds: "Ausgewählte Pakete deinstallieren",
+	// Resources Page
+	cpuHistory: "Prozessorverlauf",
+	memHistory: "Speicherverlauf",
+	networkHistory: "Netzwerkverlauf",
+	swap: "Auslagerung",
+	// Settings 
+	settings: "Einstellungen",
+	appSettings: "Anwendungseinstellungen",
+	appLanguage: "Sprache",
+	appTheme: "Theme",
+	// Global
+	search: "Suchen",
+	downloadUpdate: "Aktualisierung herunterladen",
+	updateMsg: "Es sind Aktualisierugen verfügbar.",
+	// Messages
+	opFail: "Operation fehlgeschlagen.",
+	sysCleanFail: "Die Systembereinigung schlug fehl.",
+	sysCleanSuc: "Das System wurde bereinigt.",
+	appCacheWarning: "Den Anwendungszwischenspeicher zu löschen kann Probleme verursachen. Bitte sehr vorsichtig damit sein.",
+	uninstallFail: "Deinstallation fehlgeschlagen.",
+	packageUninstalled: " Pakete deinstalliert.",
+	anotherProc: "Ein anderer Prozess läuft weiter.",
+	changedLang: "Damit die Änderungen angewandt werden können, muss das Programm neu gestartet werden.",
+	permitted: 'Diese Operation ist nicht gestattet.',
+	update: 'Aktualisieren'
+}
